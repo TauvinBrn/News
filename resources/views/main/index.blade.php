6fa41f3ea7c5e67225caddd0e97a5c7d231d7977
TauvinBrn <Vitalystolyarov@icloud.com>
2024-06-16 16:52:09 +0300

    Fusk

---
+++ resources/views/main/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts')
+
+@section('name') Главная @endsection
+
+@section('content')
+
+    <div class="d-grid gap-2 col-6 mx-auto">
+        <a href="#" class="btn btn-primary p-3">Создать новость</a>
+        <a href="#" class="btn btn-primary p-3">Все новости</a>
+    </div>
+
+@endsection
