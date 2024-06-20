937e29e71c93607779eb551687a8cb884a6e1764
TauvinBrn <Vitalystolyarov@icloud.com>
2024-06-20 18:25:36 +0300

    pink

--- resources/views/news/news.blade.php
+++ resources/views/news/news.blade.php
@@ -4,33 +4,26 @@
 
 @section('content')
     <div class="container text-center">
-        <div class="row align-items-center">
-            <div class="col">
-                <img class="figure-img img-fluid rounded" src="/storage/{{$news->images}}" alt="Img">
 
-            </div>
-            <div class="col">
+        <h3 class=" mt-5 card-title">{{ $news->title }}</h3>
+        <h5 class=" mt-3 card-text">{{ $news->description }}</h5>
+        <img class="figure-img img-fluid rounded" src="/storage/{{$news->images}}" alt="Img">
 
-                <div class="card mt-5" style="width: 30rem;">
-                    <div class="card-body">
-                        <h1 class="card-title">{{ $news->title }}</h1>
-                        <h2 class="card-text">{{ $news->description }}</h2>
-                        <p class="card-text">{{ $news->content }}</p>
-                    </div>
+        <style>
+            .img-fluid {
+                max-width: 35%;
+                height: auto;
+            }
+        </style>
 
+        <p class="card-text">{{ $news->content }}</p>
 
-                </div>
-                <a class="btn btn-primary mt-3" href="{{route('news.show')}}">Вернутся к остальным новостям</a>
-                <a class="btn btn-success mt-3" href="{{route('news.edit',$news->id)}}">Редактировать новость</a>
-                <a class="btn btn-danger mt-3" href="{{route('news.delete',$news->id) }}">Удалить нововсть</a>
-
-            </div>
-
-            <div class="col">
-                <img class="figure-img img-fluid rounded" src="/storage/{{$news->images2}}" alt="Img">
-            </div>
+        <img class="figure-img img-fluid rounded" src="/storage/{{$news->images2}}" alt="Img">
+        <div>
+            <a class="btn btn-primary mt-3" href="{{route('news.show')}}">Вернутся к остальным новостям</a>
+            <a class="btn btn-success mt-3" href="{{route('news.edit',$news->id)}}">Редактировать новость</a>
+            <a class="btn btn-danger mt-3" href="{{route('news.delete',$news->id) }}">Удалить нововсть</a>
         </div>
     </div>
-
 
 @endsection
