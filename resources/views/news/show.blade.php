5cba79a464abcd36dcff7b8458838093fd95a5fc
TauvinBrn <Vitalystolyarov@icloud.com>
2024-06-20 17:51:58 +0300

    News

--- resources/views/news/show.blade.php
+++ resources/views/news/show.blade.php
@@ -5,22 +5,21 @@
 @section('content')
 
 
-{{----}}
 <div class="container row row-cols-3 m-auto p-0">
 
     @foreach($news as $n)
     <div class="col mt-3 w-10">
-        <a href="{{ route('news.show_by_index', $n->id) }}">
-            <div class="card w-100" style="width: 18rem;">
-                <div class="card-body text-center">
-                    <h5 class="card-title ">{{ $n->title }}</h5>
-                    <small>
-                    <p class="card-text">{{ $n->description }}</p>
-                    </small>
-
+            <div class="row">
+                <div class="col-sm-19 text-center">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $n->title }}</h5>
+                            <p class="card-text max">{{$n->description}}</p>
+                            <a class="btn btn-primary" href="{{ route('news.show_by_index', $n->id) }}"> Читать новость  </a>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </a>
     </div>
     @endforeach
 
